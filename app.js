6d4a547c37a266d1a2187079b9e2ea55ea6cdfbf
digitalBrain64 <danik1991.dk@gmail.com>
2019-07-29 04:13:34 +0300

    additional CORS config

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const getRoutes = require('./routes/database/getData');
 const postRoutes = require('./routes/database/postData');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
   });
 app.get('/', (req,res,next)=>{
