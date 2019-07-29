322d791f093f0e5481b834242610c0727496961c
digitalBrain64 <danik1991.dk@gmail.com>
2019-07-29 03:02:15 +0300

    adding CORS security settings

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 3000;
 const getRoutes = require('./routes/database/getData');
 const postRoutes = require('./routes/database/postData');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.get('/', (req,res,next)=>{
     res.send("hello From Flora On Heroku");
 });
