45fe343493bb8b526114fd113175304878fcc49a
digitalBrain64 <danik1991.dk@gmail.com>
2019-08-04 19:13:25 +0300

    improvments

--- app.js
+++ app.js
@@ -2,25 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
-const getRoutes = require('./routes/database/getData');
-const postRoutes = require('./routes/database/postData');
+const getRoutes = require('./routes/database/post_routes');
+const postRoutes = require('./routes/database/get_routes');
 
+
+// bypass CORS restrictions by setting headers
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
-  });
-app.get('/', (req,res,next)=>{
-    res.send("hello From Flora On Heroku");
 });
+
+app.get('/', (req,res,next)=>{
+    res.send("hello from Flora on Heroku");
+});
+
+// parsing the req body to json
 app.use(bodyParser.json());
+
+// using routes
 app.use(getRoutes);
 app.use(postRoutes);
-console.log("server is listening");
 
-// listening on available port (set by cloud or default 3000)
+// listening on available port (set by cloud services or default 3000)
 app.listen(port, () =>{
-    console.log(`Server is listening on port ${port}`);
-  });
+  console.log(`Server is listening on port ${port}`);
+});
--- routes/database/database_functions.js
+++ routes/database/database_functions.js
@@ -10,24 +10,19 @@ const connection = mysql.createConnection("mysql://bbf377481226a0:eaef03fd@us-cd
 // last update time will be changed dynamically
 var lastUpdateTime;
 
-// adding new user to users table
-var add_user = (jsonObj)=>{
-  var sql = `INSERT INTO users (user_id, user_firstName, user_lastName, user_device_sn) VALUES ("${jsonObj.userId}","${jsonObj.userFirstName}","${jsonObj.userLastName}","${jsonObj.userDeviceSN}");`;  
-  connection.query(sql, function (err, result) {
-    if (err) 
-       throw err;
-    console.log(sql+" - record inserted");
-  });
-}
+var device_post_counter = 0;
 
-// getting the data from flora device and inserting it flora_device_data table
-var post_data_from_flora_device = (jsonObj) =>{
+
+// POST data to flora_device_data table
+function post_data_from_flora_device(jsonObj){
     var date = new Date();
     var log_time = `${date.getUTCDate()}-${date.getMonth()}-${date.getFullYear()}_${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-    var sql = `INSERT INTO flora_device_data (log_time,device_sn, latitude, longitude,satellites,pulse, battery, gps_status, bt_status, gsm_status) VALUES ("${log_time}","${jsonObj.GSTSerial}","${jsonObj.latitude}","${jsonObj.longitude}","${jsonObj.satellites}","${jsonObj.pulse}","${jsonObj.battery}%","${jsonObj.gps_status}","${jsonObj.bt_status}","${jsonObj.gsm_status}");`;  
+    var sql = `INSERT INTO 
+    flora_device_data (log_time,device_sn, latitude, longitude,satellites,pulse, battery, gps_status, bt_status, gsm_status) 
+    VALUES ("${log_time}","${jsonObj.GSTSerial}","${jsonObj.latitude}","${jsonObj.longitude}","${jsonObj.satellites}","${jsonObj.pulse}","${jsonObj.battery}","${jsonObj.gps_status}","${jsonObj.bt_status}","${jsonObj.gsm_status}");`;  
     
     // updating cache_table
-    update_user_cache_data(jsonObj, log_time);
+    update_device_cache_data(jsonObj, log_time);
 
     // sending to database
     connection.query(sql, function (err, result) {
@@ -38,9 +33,9 @@ var post_data_from_flora_device = (jsonObj) =>{
 }
 
 
-// function for updating user cache data = the most current and updated data about the device
-var update_user_cache_data = (jsonObj, updateTime) =>{
-
+// function for updating device cache data to the most current and updated data about the device
+var update_device_cache_data = (jsonObj, updateTime) =>{
+  device_post_counter++;
     // check after 6sec if lastUpdateTime == updateTime then NO new POST requests from end point: device is off
     // else: device is on
     setTimeout(function(){
@@ -48,37 +43,47 @@ var update_user_cache_data = (jsonObj, updateTime) =>{
         set_device_status(jsonObj.GSTSerial, 0); // 0 - off
         console.log("device is offline");
       }
-    },8000); // 6 sec
+    },8000); // 8 sec
 
     // setting the lastUpdateTime to current updateTime
     lastUpdateTime = updateTime;
 
-    var sql = `UPDATE cache_table
-    SET   log_time="${updateTime}",
-          device_status=1,
-          latitude="${jsonObj.latitude}",
-          longitude="${jsonObj.longitude}",
-          satellites="${jsonObj.satellites}",
-          pulse="${jsonObj.pulse}",
-          battery="${jsonObj.battery}",
-          gps_status="${jsonObj.gps_status}",
-          bt_status="${jsonObj.bt_status}",
-          gsm_status="${jsonObj.gsm_status}"
-    WHERE device_sn=${jsonObj.GSTSerial};`;
-    
-    
+    var sql = `UPDATE device_cache_data
+               SET log_time="${updateTime}",
+               device_status=1,
+               latitude="${jsonObj.latitude}",
+               longitude="${jsonObj.longitude}",
+               sats="${jsonObj.satellites}",
+               pulse="${jsonObj.pulse}",
+               battery="${jsonObj.battery}",
+               gps_status="${jsonObj.gps_status}",
+               bt_status="${jsonObj.bt_status}",
+               gsm_status="${jsonObj.gsm_status}"
+               WHERE device_sn=${jsonObj.GSTSerial};`;
+
     connection.query(sql, function (err, result) {
       if (err) 
          throw err;
       console.log("cache updated");
-      
     });
-    
+
+    // if hour has passed -> insert record to device_location_history
+    if(device_post_counter == 600){
+      var sql = `INSERT INTO 
+      device_location_history (log_time, latitude, longitude ,pulse) 
+      VALUES ("${log_time}","${jsonObj.latitude}","${jsonObj.longitude}","${jsonObj.pulse}")
+      WHERE device_sn = ${jsonObj.GSTSerial};`;
+      connection.query(sql, function (err, result) {
+        if (err) 
+           throw err;
+        console.log("location history record inserted");
+      });
+    }
 }
 
 
 
-// change device status to off/on (0/1)
+// change device status off/on (0/1)
 var set_device_status = (device_sn,deviceStatus)=>{
     var sql = `UPDATE cache_table SET device_status = ${deviceStatus} WHERE device_sn = ${device_sn}`;
     connection.query(sql, function (err, result) {
@@ -88,9 +93,9 @@ var set_device_status = (device_sn,deviceStatus)=>{
       });
 }
 
-// serving the updated data from cache table
-function get_user_data_from_cache(callback, user_id){
-  connection.query(`SELECT * FROM cache_table WHERE device_sn = 1`, function (error, result, fields) {
+// serving the latest data from device cache data
+function get_user_data_from_cache(callback, device_sn){
+  connection.query(`SELECT * FROM device_cache_data WHERE device_sn = ${device_sn}`, function (error, result, fields) {
       if (error){ 
           throw error;
       }
@@ -99,7 +104,7 @@ function get_user_data_from_cache(callback, user_id){
 }
 
 function get_user(callback,email, password){
-  var sql = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
+  var sql = `SELECT * FROM app_users WHERE user_email = '${email}' AND user_pass = '${password}'`;
 
   connection.query(sql, function(error, result, fields){
     if(error){
@@ -110,11 +115,28 @@ function get_user(callback,email, password){
   })
 }
 
+// fetching location history
+function get_location_history(callback,device_sn, from_date, to_date){
+  var sql = ` SELECT * 
+             FROM device_location_history 
+             WHERE device_sn = ${device_sn} 
+             AND log_time >= ${from_date}
+             AND log_time <= ${to_date}`
+
+  connection.query(sql, function(error, result, fields){
+    if(error){
+      throw error;
+    }
+    return callback(error, result); // return callback function
+  })
+}
+
 
 module.exports = {
     add_user,
     post_data_from_flora_device,
     get_user_data_from_cache,
-    get_user
+    get_user,
+    get_location_history
 };
   
