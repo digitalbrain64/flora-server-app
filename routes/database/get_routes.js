f3000c5f7ea3c3c4d265c54ddf354aa1cec3ecd6
digitalBrain64 <danik1991.dk@gmail.com>
2019-08-20 00:30:40 +0300

    improvements

--- routes/database/get_routes.js
+++ routes/database/get_routes.js
@@ -6,46 +6,51 @@ const database = require('./database_functions');
 
 // route to get location history from device_location_history
 router.get('/getLocationHistory', (req, res, next)=>{
-  if(!req.query.device_sn || !req.query.from_date || !req.query.to_date){
+  if(req.query.device_sn && req.query.from_date && req.query.to_date){
+    database.get_location_history(function (err, result) {
+      if (err)
+          res.send([{
+            status : "error",
+            message : err
+          }])
+      else {
+          res.send(result);          
+      }
+    },req.query.device_sn, req.query.from_date, req.query.to_date);
+  }
+  else{
     res.send([{
       status : "error",
       message : "please provide parameters: device id, from date, to date"
     }])
   }
-  database.get_location_history(function (err, result) {
-    if (err)
-        res.send([{
-          status : "error",
-          message : err
-        }])
-    else {
-        res.send(result);
-        console.log(`results found: ${result.length}\n#########`);
-        
-    }
-  },req.query.device_sn, req.query.from_date, req.query.to_date);
 });
 
 // route to get recent data from device_cache_data
 router.get('/getDataFromCache', (req, res, next) =>{
-  console.log("fetching data for device_sn: "+req.query.device_sn);
-  database.get_user_data_from_cache(function (err, result) {
-      if (err)
-        console.log("Database error!");
+  if(req.query.device_sn){
+    database.get_user_data_from_cache(function (err, result) {
+      if (err){
+        res.send([{
+          status : "error",
+          message : err
+        }])
+      }
       else
         res.send(result);
-      next();
-  }, req.query.device_sn);
+    }, req.query.device_sn);
+  }
+  else{
+    res.send([{
+      status : "error",
+      message : "please provide device serial number"
+    }])
+  }
 });
 
 // route to get user from app_users
 router.get('/getUserData', (req, res, next)=>{
-    if(!req.query.p && !req.query.u)
-      res.send([{
-        status : "error",
-        message : "please provide password and username/email"
-      }])
-    else{
+    if(req.query.p && req.query.u){
       database.get_user(function(err, results){
         if(err)
           res.send([{
@@ -55,20 +60,35 @@ router.get('/getUserData', (req, res, next)=>{
         else
           res.send(results);
       }, req.query.u, req.query.p)
+    }
+    else{
+      res.send([{
+        status : "error",
+        message : "please provide password and username/email"
+      }])
     } 
 });
 
 router.get('/getWeatherUpdate', (req, res, next)=>{
-  database.get_weather_update(function(err, result){
-    if(err)
-      res.send([{
-        status : "error",
-        message : err
-      }]);
-    else
-      res.send(result);
-    next();
-  }, req.query.lat, req.query.lng)
+  if(req.query.lat && req.query.lng){
+    database.get_weather_update(function(err, result){
+      if(err)
+        res.send([{
+          status : "error",
+          message : err
+        }]);
+      else
+        res.send(result);
+      next();
+    }, req.query.lat, req.query.lng)
+  }
+  else{
+    res.send([{
+      status:"error",
+      message:"please provide latitude and longitude"
+    }])
+  }
+  
 })
 
 
@@ -102,35 +122,46 @@ router.get('/sendRestoreCode', (req, res, next)=>{
 // validating the code and email if user exists
 // if all went well : email sent back to client for later use in the final step
 router.get('/checkRestoreCode', (req, res, next)=>{
-  var code = req.query.restoreCode;
-  var email = req.query.e;
-
-  database.check_restore_code(function(err, result){
-    if(err)
-      res.send([{
-        status : "error",
-        message : err
-      }])
-    else
-      res.send(result)
-  },email,code)
+  if(req.query.restoreCode && req.query.e){
+    database.check_restore_code(function(err, result){
+      if(err)
+        res.send([{
+          status : "error",
+          message : err
+        }])
+      else
+        res.send(result)
+    },req.query.e,req.query.restoreCode)
+  }
+  else{
+    res.send([{
+      status : "error",
+      message : "please provide a restore code and email"
+    }])
+  }
 })
 
 // 3 : sending the new password and email from previous step
 // updating the password and removing the restore_code from user in database
 router.get('/passChange', (req, res, next)=>{
-  var newPass = req.query.newPass;
-  var user_email = req.query.e;
-  database.change_user_pass(function(err, result){
-    if(err)
-      res.send([{
-        status : "error",
-        message : err
-      }])
-      else{
-        res.send(result)
-      }
-  },user_email, newPass)
+  if(req.query.newPass && req.query.e){
+    database.change_user_pass(function(err, result){
+      if(err)
+        res.send([{
+          status : "error",
+          message : err
+        }])
+        else{
+          res.send(result)
+        }
+    },req.query.e, req.query.newPass)
+  }
+  else{
+    res.send([{
+      status : "error",
+      message : "please provide new password and email"
+    }])
+  }
 })
 
 router.get('/getLowestPulse', (req, res, next)=>{
